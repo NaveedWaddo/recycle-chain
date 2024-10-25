e69f6eb9d0b6ce63c9086248f9318c702195a40b
Naveed Waddo <naveedwaddo1123@gmail.com>
2024-10-25 16:37:59 +0530

    Create recycle chain smart contract

--- apps/event-listener/util.ts
+++ apps/event-listener/util.ts
@@ -1,1 +1,1 @@
-export const contractAddress = '0x967161e6e389Fa7835EEa9724194253642B88Ae3'
+export const contractAddress = '0x675D462c9d0ae83eeC532B1FB099FF79996EBaFF'
--- standalone/simple-counter/contracts/SimpleCounter.sol
+++ standalone/simple-counter/contracts/SimpleCounter.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.24;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract SimpleCounter{
+contract SimpleCounter {
     uint256 public number;
 
-    constructor(uint256 initialNumber){
+    constructor(uint256 initialNumber) {
         number = initialNumber;
     }
 
